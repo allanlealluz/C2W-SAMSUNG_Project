91b85fb20d98fac042b9afb094fe4e104e9ba2da
Allan <69430214+allanlealluz@users.noreply.github.com>
2024-08-16 19:54:11 -0300

    Add files via upload

--- test.py
+++ test.py
@@ -4,6 +4,9 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("home.html")
+@app.route("/cadastro/<nomes>")
+def cadastro(nomes):
+    return render_template("cadastro.html", nomes = nomes)
 if __name__ == "__main__":
     app.run(debug=True)
 
