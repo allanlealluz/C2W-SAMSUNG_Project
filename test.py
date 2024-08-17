cbf2fd9cb6d85c2390bd6b3cc9e0879b3625ad14
allanlealluz <allanlealluz@gmail.com>
2024-08-17 14:29:59 -0300

    ..

--- test.py
+++ test.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("home.html")
-@app.route("/cadastro/<nomes>")
-def cadastro(nomes):
-    return render_template("cadastro.html", nomes = nomes)
+@app.route("/cadastro")
+def cadastro():
+    return render_template("cadastro.html")
 if __name__ == "__main__":
     app.run(debug=True)
 
