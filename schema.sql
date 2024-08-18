e2640288893a195134c618fd7119f7722cd152b5
allanlealluz <allanlealluz@gmail.com>
2024-08-18 14:13:09 -0300

    changes 18-08 6

---
+++ schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS dados (
+    id INT,
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    telefone VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+)
--- test.py
+++ test.py
@@ -1,9 +1,26 @@
 from flask import Flask,render_template
+import sqlite3
 app = Flask(__name__)
 
+DATABASE = 'database.db'
+def get_db():
+    db = sqlite3.connect(DATABASE)
+    db.row_factory = sqlite3.Row
+    return db
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql',mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
 @app.route("/")
 def index():
     return render_template("home.html")
+  
+@app.route("/initdb")
+def initiate_database():
+    init_db()
+    return "DATABASE CONNECTED"
 @app.route("/cadastro")
 def cadastro():
     return render_template("cadastro.html")
