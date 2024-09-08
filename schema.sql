e6769ccfdefa567bd5434a94d3b8fe180b7f8706
allanlealluz <allanlealluz@gmail.com>
2024-09-08 18:02:53 -0300

    changes 08-09 1

--- schema.sql
+++ schema.sql
@@ -3,5 +3,6 @@ CREATE TABLE IF NOT EXISTS usuarios (
     nome VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     senha varchar(255) NOT NULL,
+    tipo TEXT NOT NULL,
     PRIMARY KEY (id)
 )
--- test.py
+++ test.py
@@ -32,37 +32,46 @@ def initiate_database():
     init_db()
     return "DATABASE CONNECTED"
 #pagina de cadastro
+# Página de cadastro
 @app.route("/cadastro", methods=["POST", "GET"])
 def cadastro():
     if request.method == "POST":
         nome = request.form['nome']
         email = request.form['email']
         senha = sha256(request.form['senha'].encode('utf-8')).hexdigest()
+        tipo = request.form['tipo']  # Novo campo para o tipo de usuário
 
         db = get_db()
-        db.execute('INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)', (nome, email, senha))
+        db.execute('INSERT INTO usuarios (nome, email, senha, tipo) VALUES (?, ?, ?, ?)', (nome, email, senha, tipo))
         db.commit()
 
         return render_template("login.html")
 
     return render_template("cadastro.html")
+
 #pagina de login
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
         email = request.form['email']
-        senha =  sha256(request.form['senha'].encode('utf-8')).hexdigest()
+        senha = sha256(request.form['senha'].encode('utf-8')).hexdigest()
 
         db = get_db()
         user = db.execute('SELECT * FROM usuarios WHERE email = ?', (email,)).fetchone()
 
-        if user and senha:
+        if user and user['senha'] == senha:
             session["user"] = email
-            return render_template("main.html")
+            session["tipo"] = user['tipo']  # Salvando o tipo de usuário na sessão
+
+            if user['tipo'] == 'professor':
+                return redirect(url_for('dashboard_professor'))
+            else:
+                return redirect(url_for('dashboard_aluno'))
         else:
-            return jsonify({"message": "Credenciais invalidas!"})
+            return jsonify({"message": "Credenciais inválidas!"})
 
     return render_template("login.html")
+
 @app.route('/main')
 def main_page():
     # Verifica se o usuário está logado
