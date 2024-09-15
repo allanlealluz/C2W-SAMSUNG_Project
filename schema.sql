675f9f4b7bfb4e3bf29a236e24c57d021f2cdfa2
allanlealluz <allanlealluz@gmail.com>
2024-09-15 12:15:54 -0300

    changes 15-09 2

--- schema.sql
+++ schema.sql
@@ -5,4 +5,12 @@ CREATE TABLE IF NOT EXISTS usuarios (
     senha varchar(255) NOT NULL,
     tipo TEXT NOT NULL,
     PRIMARY KEY (id)
-)
+);
+CREATE TABLE IF NOT EXISTS respostas (
+    id INT,
+    user_id INT NOT NULL,
+    section VARCHAR(255) NOT NULL,
+    response TEXT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES usuarios(id)
+);
