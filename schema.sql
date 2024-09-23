e1790b01d71f5aa796d64398a6574146a59e3b68
allanlealluz <allanlealluz@gmail.com>
2024-09-23 12:40:35 -0300

    changes 23-09 1

--- schema.sql
+++ schema.sql
@@ -10,13 +10,21 @@ CREATE TABLE IF NOT EXISTS respostas (
     user_id INT NOT NULL,
     section VARCHAR(255) NOT NULL,
     response TEXT NOT NULL,
+    aula_id INTEGER NOT NULL,  -- Adicionando coluna aula_id
     FOREIGN KEY (user_id) REFERENCES usuarios(id)
 );
+
 CREATE TABLE IF NOT EXISTS progresso_atividades (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER NOT NULL,
     section_id INTEGER NOT NULL,
+    aula_id INTEGER NOT NULL,  -- Adicionando coluna aula_id
     completou BOOLEAN NOT NULL DEFAULT 0,
-    UNIQUE(user_id, section_id),
+    UNIQUE(user_id, section_id, aula_id),  -- Atualizando a restrição UNIQUE
     FOREIGN KEY(user_id) REFERENCES usuarios(id)
+);
+CREATE TABLE IF NOT EXISTS aulas (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome VARCHAR(255) NOT NULL,
+    descricao TEXT
 );
