f2c10a261435d1d2ff951d6230365753dbccb57c
allanlealluz <allanlealluz@gmail.com>
2024-09-25 15:23:20 -0300

    changing the project structure part 4

--- routes/teacher_routes.py
+++ routes/teacher_routes.py
@@ -1,9 +1,16 @@
-from flask import Blueprint, render_template, session
-from models import get_db
+from flask import Blueprint, render_template, session, redirect, url_for
+from models import get_db, find_user_by_id
 from utils import generate_plot
 
 teacher_bp = Blueprint('teacher', __name__)
 
+@teacher_bp.route('/dashboard_professor')
+def dashboard_professor():
+    user_id = session.get("user")
+    if user_id and session['tipo'] == 'professor':
+         userData = find_user_by_id(user_id) 
+         return render_template('dashboard_professor.html',user=userData)
+    return redirect(url_for('auth.login'))     
 @teacher_bp.route('/dashboard_professor/feedbacks')
 def ver_feedbacks():
     if 'user' in session and session['tipo'] == 'professor':
