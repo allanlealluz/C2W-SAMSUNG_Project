ad3039f1132d727283083920083cce135b697da2
AlephGoldstein <allanlealluz@gmail.com>
2024-11-05 21:11:56 -0300

    ..

--- schema.sql
+++ schema.sql
@@ -1,5 +1,3 @@
-INSERT INTO usuarios (nome, email, senha, tipo) 
-VALUES ('Allan', 'admin@gmail.com', 'allone', 'admin');
 CREATE TABLE IF NOT EXISTS usuarios (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     nome VARCHAR(255) NOT NULL,
